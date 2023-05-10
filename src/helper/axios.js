8b93d7c9352838fd8e75830ff892a8d1a2362124
TuanVo <tuan.vo@alttekglobal.com>
2023-05-10 17:31:49 +0700

    fix build script

--- src/helper/axios.js
+++ src/helper/axios.js
@@ -4,7 +4,7 @@ const token = window.localStorage.getItem("token");
 //console.log(token);
 const testPort = "http://192.168.137.206:8888";
 const axiosIntance = axios.create({
-  baseURL: "http://localhost:8090/api/",
+  baseURL: "https://be-production-9000.up.railway.app/api/",
   headers: {
     Authorization: token ? `Bearer ${token}` : "",
   },
