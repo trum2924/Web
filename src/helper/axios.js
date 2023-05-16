2f97ae3dc045f5a543ba5053f218bd967ce74711
oVTuan <trum2924@gmail.com>
2023-05-16 17:32:49 +0700

    Update axios.js

--- src/helper/axios.js
+++ src/helper/axios.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 const token = window.localStorage.getItem("token");
 //console.log(token);
-const testPort = "http://192.168.137.206:8090";
+const testPort = "https://be-production-9000.up.railway.app";
 const axiosIntance = axios.create({
-  baseURL: "http://192.168.137.206:8090/api/",
+  baseURL: "https://be-production-9000.up.railway.app/api/",
   headers: {
     Authorization: token ? `Bearer ${token}` : "",
   },
